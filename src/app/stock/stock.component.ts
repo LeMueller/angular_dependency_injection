c4148e109e7dd93db48536bde0a6098502ef70c4
Le Zhang <ZhangL@duerkopp-adler.com>
2018-07-03 12:11:34 +0200

    injection

--- src/app/stock/stock.component.ts
+++ src/app/stock/stock.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
 import { StockService, Stock } from '../shared/stock.service';
 
 @Component({
@@ -10,9 +10,19 @@ export class StockComponent implements OnInit {
 
   private stock: Stock;
 
+  // 手动进行注入器注入。只是解释，项目中不要真这么写！！！
+  public stockService: StockService;
+
+  // 构造函数的参数是注入器需要注入的服务，如果没有，就不需要任何注入
   constructor(
-    public stockService: StockService
-  ) { }
+    // public stockService: StockService
+
+    // 手动进行注入器注入。只是解释，项目中不要真这么写！！！
+    public injector: Injector
+  ) {
+    // 手动进行注入器注入。只是解释，项目中不要真这么写！！！
+    this.stockService = injector.get(StockService);
+  }
 
   ngOnInit() {
     this.stock = this.stockService.getStock();
