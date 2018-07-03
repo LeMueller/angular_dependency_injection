51f00edd945d2824bbe84a5c98c588ea761b913d
Le Zhang <ZhangL@duerkopp-adler.com>
2018-07-03 11:36:25 +0200

    providers useClass, useFactory, useValue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StockComponent } from './stock/stock.component';
 import { StockService } from './shared/stock.service';
 import { Stock2Component } from './stock2/stock2.component';
 import { LoggerService } from './shared/logger.service';
+import { AnotherStockService } from './shared/another-stock.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,24 @@ import { LoggerService } from './shared/logger.service';
     BrowserModule
   ],
   // 在没有加入StockService时，并没有报错。因为是Angular6，不是Angular4?
-  providers: [StockService, LoggerService],
+  providers: [
+    // 工厂方法只是在第一次被调用的时候，实例化一次，之后都用这个一个实例
+    {provide: StockService, useFactory: (logger: LoggerService, isDev) => {
+      console.log('isDev: ', isDev);
+      
+      // let logger = new LoggerService();
+      // let dev = Math.random() > 0.5;
+      if(isDev) {
+        return new StockService(logger);
+      }
+      else {
+        return new AnotherStockService(logger);
+      }
+      // deps: 工厂模式匿名函数的参数用到的provider
+    }, deps: [LoggerService, "IS_DEV_ENV"]},
+    LoggerService,
+    {provide: "IS_DEV_ENV",  useValue: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/stock2/stock2.component.ts
+++ src/app/stock2/stock2.component.ts
@@ -6,7 +6,7 @@ import { StockService, Stock } from '../shared/stock.service';
   selector: 'app-stock2',
   templateUrl: './stock2.component.html',
   styleUrls: ['./stock2.component.css'],
-  providers: [{provide: StockService, useClass: AnotherStockService}]
+  // providers: [{provide: StockService, useClass: AnotherStockService}]
 })
 export class Stock2Component implements OnInit {
 
